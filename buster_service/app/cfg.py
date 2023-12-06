ac18a1692e157d4b2250ad75dac3c3509388996b
Olivier Breuleux <breuleux@gmail.com>
2023-12-06 14:01:53 -0500

    Move imports inside of cfg

--- buster_service/app/cfg.py
+++ buster_service/app/cfg.py
@@ -1,18 +1,10 @@
 import logging
-
-import openai
-from buster.busterbot import Buster, BusterConfig
-from buster.completers import ChatGPTCompleter, DocumentAnswerer
-from buster.formatters.documents import DocumentsFormatterHTML
-from buster.formatters.prompts import PromptFormatter
-from buster.retriever import DeepLakeRetriever, Retriever
-from buster.tokenizers import GPTTokenizer
-from buster.validators import Validator
+import os
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-buster: Buster = None
+buster = None
 config: dict = None
 
 
@@ -21,7 +13,15 @@ def configure(cfg):
     config = cfg
 
     # set openAI creds
-    openai.api_key = config["openai_api_key"]
+    os.environ["OPENAI_API_KEY"] = config["openai_api_key"]
+
+    from buster.busterbot import Buster, BusterConfig
+    from buster.completers import ChatGPTCompleter, DocumentAnswerer
+    from buster.formatters.documents import DocumentsFormatterHTML
+    from buster.formatters.prompts import PromptFormatter
+    from buster.retriever import DeepLakeRetriever, Retriever
+    from buster.tokenizers import GPTTokenizer
+    from buster.validators import Validator
 
     buster_cfg = BusterConfig(**cfg["buster"])
 
